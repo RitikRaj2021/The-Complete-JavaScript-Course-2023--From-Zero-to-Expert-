d92dc8a6ac5124dc037a30403138629f6b96e300
Ritik Raj <ritikr379@gmail.com>
2023-08-10 11:48:58 +1200

    finished section 1

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -276,4 +276,93 @@ if (favourite !== 23) console.log("Why not 23?");
 
 //strict not equal operator
 
+const hasDriversLicense = true; // A
+const hasGoodVision = true; // B
+
+console.log(hasDriversLicense && hasGoodVision); // AND\
+console.log(hasDriversLicense || hasGoodVision); // OR
+console.log(!hasDriversLicense); // NOT
+
+
+// if (hasDriversLicense && hasGoodVision) {
+//     console.log("Sarah is able to drive!");
+// } else {
+//     console.log("Someone else should drive...");
+// }
+
+const isTired = false; // C
+console.log(hasDriversLicense || hasGoodVision || isTired);
+
+if (hasDriversLicense && hasGoodVision && !isTired) {
+    console.log("Sarah is able to drive!");
+} else {
+    console.log("Someone else should drive...");
+}
+
+
+const scoreDolphins = (96 + 108 + 89) / 3;
+const scoreKoalas = (88 + 91 + 110) / 3;
+console.log(scoreDolphins, scoreKoalas);
+
+if (scoreDolphins > scoreKoalas) {
+    console.log("Dolphins win the trophy");
+} else if (scoreKoalas > scoreDolphins) {
+    console.log("Koalas win the trophy");
+} else if (scoreDolphins === scoreKoalas) {
+    console.log("Both win the trophy");
+}
+
+
+const day = "friday";
+console.log(day);
+
+switch (day) { 
+    case "monday": // day === "monday"
+        console.log("Plan course structure");
+        console.log("Go to coding meetup");
+        break;
+    case "tuesday":
+        console.log("Prepare theory videos");
+        break;
+    case "wednesday":
+    case "thursday":
+        console.log("Write code examples");
+        break;
+    case "friday":
+        console.log("Record videos");
+        break;
+    case "saturday":
+    case "sunday":
+        console.log("Enjoy the weekend :D");
+        break;
+    default:
+        console.log("Not a valid day!");
+}
+
+
+if (day === "monday") {
+}else if (day === "tuesday") {
+    console.log("Prepare theory videos");
+} else if (day === "wednesday" || day === "thursday") {
+    console.log("Write code examples");
+} else if (day === "friday") {
+    console.log("Record videos");
+} else if (day === "saturday" || day === "sunday") {
+    console.log("Enjoy the weekend :D");
+} else {
+    console.log("Not a valid day!");
+}
+
+
+3 + 4
+1991
+true && false && !false
+
+if (23 > 10) {
+    const str = "23 is bigger";
+}
 */
+
+// const age = 23;
+// age >= 18 ? console.log("I like to drink wine") : console.log("I like to drink water");
+
